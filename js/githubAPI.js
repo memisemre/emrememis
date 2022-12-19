dd69f6754b429cde4cc385bc1e742c6ab028756a
softyak <rsat.memis@gmail.com>
2022-12-19 23:33:43 +0300

    a little refactor

--- js/githubAPI.js
+++ js/githubAPI.js
@@ -1,18 +1,19 @@
-import { GITHUB_REPOS , GITHUB_STARS , GITHUB_FOLLOWERS } from "./selectors.js";
+import * as SELECTORS from "./selectors.js";
+import { select } from "./utils.js";
 let stars = 0;
 export function githubAPI(){
       fetch("https://api.github.com/users/memisemre")
       .then(res=>res.json())
       .then((res)=>{
-            GITHUB_REPOS.innerHTML = `${res.public_repos}`;
-            GITHUB_FOLLOWERS.innerHTML = `${res.followers}`;
+            select(SELECTORS.GITHUB_REPOS).innerHTML = `${res.public_repos}`;
+            select(SELECTORS.GITHUB_FOLLOWERS).innerHTML = `${res.followers}`;
       })
       fetch("https://api.github.com/users/memisemre/repos")
       .then(res=>res.json())
       .then((res)=>{
             res.forEach((repo)=>{
                   stars = stars + repo.stargazers_count;
-                  GITHUB_STARS.innerHTML = `${stars}`;
+                  select(SELECTORS.GITHUB_STARS).innerHTML = `${stars}`;
             })
       })
 }
